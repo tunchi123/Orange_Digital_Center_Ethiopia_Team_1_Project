85800d115e5e5c094a528febf6817b9ddf292491
tunchi123 <107255210+tunchi123@users.noreply.github.com>
2022-09-21 12:46:17 +0300

    power function for calculator

---
+++ power.py
@@ -0,0 +1,9 @@
+def power(a,b):
+
+    """ Power function calculator"""
+    
+    return (a ** b)
+
+a = int(input("please enter number "))
+b = int(input("please enter number of power "))
+print(power(a, b))
